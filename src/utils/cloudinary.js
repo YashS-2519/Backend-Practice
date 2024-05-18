a82db2089b0092d154718f6ee40644fa3aaf2b9f
YashS-2519 <y.singh2519@gmail.com>
2024-05-18 12:56:00 +0530

    file config setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import { resourceLimits } from 'worker_threads';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uplodeOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.v2.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    })
+    console.log("File is uploded on cloudinary", response.url);
+  } catch (error) {
+    fs.unlinkSync(localFilePath)  //remove locally saved temporary file as the uplode operation got failed.
+    return null;
+  }
+}
+
+export {uplodeOnCloudinary}
+
+
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) { console.log(result); });
